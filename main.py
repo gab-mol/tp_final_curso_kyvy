c91a4197d3d84ce3c5863d1e497360460907c4bf
Gabriel Molina <g-abox@hotmail.com>
2023-12-02 21:18:11 -0300

    update funcional

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from kivymd.utils import asynckivy
 from kivymd.uix.refreshlayout import refreshlayout
 
 import os
-from peewee import  SqliteDatabase, Model, TimestampField, CharField, TextField
+from peewee import SqliteDatabase, Model, TimestampField, CharField, TextField
 from datetime import datetime as dt
-import time
+import time as t
 import textwrap as tw
+import pandas as pd
 
 # Cargar Vista
 Builder.load_file("vista.kv")
@@ -44,13 +45,12 @@ class DbAdm:
     def __init__(self):
         self.tb = NotesDb
 
-    def alta(self, user:str, title:str, note:str):
-        
-        time.sleep(1)
-        # ALTA en SQLite
+    def alta(self, user:str, title:str, note:str, 
+            timestamp=dt.now()):
+        t.sleep(1)
         try:
             self.tb.create(
-                timestamp = dt.now(),
+                timestamp = timestamp,
                 user = user,
                 title = title,
                 note = note
@@ -63,11 +63,16 @@ class DbAdm:
         print("borrar nota del:",time_id)
         rm=self.tb.get(self.tb.timestamp == time_id)
         rm.delete_instance()
-        #time.sleep(1)
         
-    def update(self,time_id):
+    def update(self,user, time_id, title, note):
         print("cambiar esto:",time_id)
-    
+        upd=self.tb.update(
+            user = user,
+            title = title,
+            note = note
+        ).where(self.tb.timestamp == time_id)
+        upd.execute()
+            
     def read_all_items(self):
         return self.tb.select()
 
@@ -77,40 +82,52 @@ class DbAdm:
             print(int(round(fila.timestamp.timestamp())))
 
     def read(self, timestamp:int):
-        r = self.tb.get(self.tb.timestamp==timestamp)
-        print(r)
+        item = self.tb.get(self.tb.timestamp==timestamp)
+        response = {
+            "user": item.user, 
+            "title":item.title, 
+            "note":item.note
+        }
+        return response
 
 
 # Clases Kivy ###############################################################
-user = ""
-
 
 class ScreenAdm(ScreenManager):
+    id_pointer = StringProperty()
     name_str = StringProperty()
     titl_note = StringProperty()
     text_note = StringProperty()
     
+    upd_id = StringProperty()
     def __init__(self, conn:DbAdm, **kwargs):
         super().__init__(**kwargs)
         self.conn= conn
-        
-    def ver_nomb(self):
-        global user
-        print("nombre:")
-        print(self.name_str)
-        user = (self.name_str)
 
     def save_note(self):
-        global user
-        print("guardar",user, self.titl_note,self.text_note)
+        print("guardar:", self.name_str, self.titl_note, self.text_note)
+        
         self.conn.alta(
-            user=user,
+            timestamp= self.id_pointer,
+            user=self.name_str,
             title=self.titl_note,
             note=self.text_note
         )
+        
+    def change_note(self, title,note):
+        timestamp=  dt.strptime(self.id_pointer,"%Y-%m-%d %H:%M:%S")
+        print("RESULT",timestamp, type(timestamp))
+        self.conn.update(
+            user= self.name_str,
+            time_id= timestamp,
+            title= title,
+            note= note
+        )
+
 
 class Note(MDCard):
     text = StringProperty()
+    id = StringProperty()
     
     def __init__(self, conn:DbAdm, id_card, **kwargs):
         super().__init__(**kwargs)
@@ -119,9 +136,11 @@ class Note(MDCard):
     
     def _del(self):
         self.conn.delete(self.id_card)
-    
+
     def _upd(self):
-       self.conn.update(self.id_card)
+        id_ts = str(self.id_card)
+        print("ESTO",self.id_card)
+        self.id = id_ts
 
 
 class Log(Screen): pass
@@ -131,6 +150,13 @@ class NoteList(Screen): pass
 
 
 class WritNote(Screen): pass
+
+
+class UpdNote(Screen):
+    upd_title = StringProperty()
+    upd_note = StringProperty()
+    def __init__(self, **kw):
+        super().__init__(**kw)
 
 
 class MainApp(MDApp):
@@ -156,7 +182,7 @@ class MainApp(MDApp):
             self.root.ids.md_list.add_widget(
                 Note(
                     id_card= it.timestamp,
-                    text=f"{it.title} | Por: {it.user}, \
+                    text=f"[b]{it.title}[/b] | Por: {it.user}, \
 {it.timestamp}\n {note_form}", 
                     conn=self.conn
                 )
